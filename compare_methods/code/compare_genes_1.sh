aacb5500a27115772dfd8e7f28b766270452e172
alextidd <alexandrarubytidd@gmail.com>
2022-04-11 16:58:31 +1000

    add protein-coding feature, change plotting

--- compare_methods/code/compare_genes_1.sh
+++ compare_methods/code/compare_genes_1.sh
@@ -2,42 +2,33 @@
 module load bedtools/2.27.1
 module load R/4.0.2
 
-(while IFS=$'\t' read -r trait variants variants_file known_genes_file ; do
+base_dir=/working/lab_jonathb/alexandT/ ; 
+WKDIR=$base_dir/tgp_paper/compare_methods/ ; cd $WKDIR
+traits_metadata=$base_dir/tgp_paper/wrangle_package_data/traits/output/metadata.tsv
 
+(while IFS=$'\t' read -r trait variants variants_file known_genes_file ; do 
   echo $variants
-  base_dir=/working/lab_jonathb/alexandT/ ; 
-  WKDIR=$base_dir/tgp_paper/compare_methods/ ; cd $WKDIR
   out_dir=output/$variants/ ; mkdir -p $out_dir
-  
   variants_bed=$base_dir/tgp_paper/wrangle_package_data/traits/output/$variants/variants.bed
-  traits_metadata=$base_dir/tgp_paper/wrangle_package_data/traits/output/metadata.tsv
   #PMID=$(cat $traits_metadata | awk -v trait="$trait" '$2 == trait {print $5}')
   #EpiMAP_BSS=$(cat data/EpiMAP/metadata/main_metadata_table.tsv | awk '$3=="MCF-7" {print $1}')
   
   (
     echo -e "variant\tcs\tsymbol\tscore\tmethod" ;
       
-    # tgp enriched_celltypes #
+    # tgp_enriched_celltypes #
     cat $base_dir/tgp/out/$variants/enriched_celltypes/target_gene_predictions_full.tsv |
     awk -F'\t' -vOFS='\t' 'NR > 1 && $7 > 0 {print $5,$1,$6,$7,"tgp_enriched_celltypes"}' |
     sort -u ;
     
-    # tgp enriched tissues #
+    # tgp_enriched_tissues #
     cat $base_dir/tgp/out/$variants/enriched_tissues/target_gene_predictions_full.tsv |
     awk -F'\t' -vOFS='\t' 'NR > 1 && $7 > 0 {print $5,$1,$6,$7,"tgp_enriched_tissues"}' |
     sort -u ;
     
-    # tgp all celltypes #
+    # tgp_all_celltypes #
     cat $base_dir/tgp/out/$variants/all_celltypes/target_gene_predictions_full.tsv |
     awk -F'\t' -vOFS='\t' 'NR > 1 && $7 > 0 {print $5,$1,$6,$7,"tgp_all_celltypes"}' |
-    sort -u ;
-    
-    # closest #
-    bedtools closest \
-      -a $variants_bed  \
-      -b <(zcat $base_dir/tgp_paper/wrangle_package_data/reference_panels/data/GENCODE/gencode.v34lift37.basic.tss.bed.gz) |
-    cut -f4,5,10 |
-    awk '{print $0"\t1\tclosest"}' | 
     sort -u ;
     
     # ABC all celltypes #
@@ -45,7 +36,7 @@ module load R/4.0.2
     # when an enhancer was interacting with a gene in multiple cell-types, and used this 
     # score as raw linking value.
     zcat data/ABC/ABC_AllPredictions_collapsed.bed.gz |
-    bedtools intersect -a $variants_bed -b - -wa -wb |
+    bedtools intersect -a <(cut -f1-5 $variants_bed) -b - -wa -wb |
     cut -f4,5,9,10 |
     awk '{print $0"\tABC_all_celltypes"}' | 
     sort -u ;
@@ -63,7 +54,7 @@ module load R/4.0.2
     # zcat data/ABC/AllPredictions.AvgHiC.ABC0.015.minus150.ForABCPaperV3.txt.gz | 
     # awk '$24 == "MCF-7-ENCODE"' |
     # sort -k1,1 -k2,2n |
-    # bedtools intersect -a $variants_bed -b - -wa -wb |
+    # bedtools intersect -a <(cut -f1-5 $variants_bed) -b - -wa -wb |
     # awk -F'\t' -vOFS='\t' '{print $1,$2,$3,$5,$12,$26,"ABC"}' | 
     # sort -u ;
     
@@ -74,14 +65,14 @@ module load R/4.0.2
     # value.
     # must run data/EpiMAP/links/EpiMAP_corrlinks_collapsed.sh to generate collapsed links for intersection
     zcat data/EpiMAP/links/EpiMAP_corrlinks_collapsed.bed.gz |
-    bedtools intersect -a - -b $variants_bed -wa -wb | 
+    bedtools intersect -a - -b <(cut -f1-5 $variants_bed) -wa -wb | 
     awk -F'\t' -vOFS='\t' '{print $9,$10,$4,$5,"EpiMAP_all_celltypes"}' |
     sort -u ;
     
     # # EpiMAP #
     # join -1 9 -2 4 -t$'\t' -o 1.1,1.2,1.3,1.5,2.5,1.10 \
     # <(  zcat data/EpiMAP/links/links_corr_only/${EpiMAP_BSS}_collated_pred.tsv.gz |
-    #     bedtools intersect -a $variants_bed -b - -wa -wb | sort -k9,9 ) \
+    #     bedtools intersect -a <(cut -f1-5 $variants_bed) -b - -wa -wb | sort -k9,9 ) \
     # <(  zcat $base_dir/tgp_paper/wrangle_package_data/reference_panels/data/GENCODE/gencode.v34lift37.basic.tss.bed.gz |
     #     sort -k4,4 ) |
     # awk '{print $0"\tEpiMAP"}' | 
@@ -100,7 +91,7 @@ module load R/4.0.2
   
     # MAGMA # maximum positive Z score
     bedtools intersect \
-      -a $variants_bed \
+      -a <(cut -f1-5 $variants_bed) \
       -b <(awk 'NR>1 {print "chr"$2"\t"$3"\t"$4"\t"$0}' data/MAGMA/$variants/magma.genes.out | sort -k1,1 -k2,2n) \
       -wa -wb |
     awk -F'\t' -vOFS='\t' '$16 !~ "-" {print $4,$5,$18,$16,"MAGMA"}' |
--- compare_methods/code/compare_genes_2.R
+++ compare_methods/code/compare_genes_2.R
@@ -8,17 +8,6 @@ base_dir <- "/working/lab_jonathb/alexandT/tgp_paper/"
 wkdir <- paste0(base_dir, "compare_methods/") ; setwd(wkdir)
 
 # functions ====
-get_group_conusion_mat <- function(cxg_predictions){
-  cxg_predictions %>%
-  group_modify(
-    ~ data.frame(True = .x %>% condition_n_gene_x_cs_pairs(known_gene),
-                 Positive = .x %>% condition_n_gene_x_cs_pairs(max),
-                 TP = .x %>% condition_n_gene_x_cs_pairs(max & known_gene),
-                 FP = .x %>% condition_n_gene_x_cs_pairs(max & !known_gene),
-                 FN = .x %>% condition_n_gene_x_cs_pairs(!max & known_gene),
-                 TN = .x %>% condition_n_gene_x_cs_pairs(!max & !known_gene))
-  ) 
-}
 get_performance <- function(confusion_mat, 
                             variants_name){
   confusion_mat %>%
@@ -35,72 +24,17 @@ get_performance <- function(confusion_mat,
            method_name = method %>% gsub("\\_.*", "", .)) %>%
     select(variants, method_name, method, everything())
 }
-plot_performance <- function(metadata,
-                             performance,
-                             variants_name,
-                             max_n_known_genes_per_CS,
-                             variant_to_gene_max_distance){
-  colours <- metadata$colour ; names(colours) <- metadata$method
-  plot_settings <- list(
-    scale_colour_manual(values = colours, limits = force),
-    scale_fill_manual(values = colours, limits = force),
-    labs(x = paste0("recall (True = ", 
-                    unique(performance$True), 
-                    ")")),
-    theme_classic(),
-    theme(
-      title = element_text(size = 15),
-      axis.title = element_text(size = 15),
-      axis.text = element_text(size = 15)))
-  plot_subtitle <- paste0(
-    "Variants = ", variants_name,
-    "\nmax n known genes per CS = ", max_n_known_genes_per_CS, 
-    ", max distance = ", variant_to_gene_max_distance)
-  
-  # plots
-  performance_plot <- performance %>%
-    mutate(fsc = F_score) %>%
-    pivot_longer(cols = c(F_score, Precision, Recall),
-                 names_to = "metric",
-                 values_to = "performance") %>%
-    ggplot(aes(x = reorder(method, fsc),
-               y = performance,
-               fill = method_name)) +
-    geom_col() +
-    facet_grid(~ metric) +
-    coord_flip() +
-    plot_settings +
-    theme(legend.position = "none",
-          axis.title.y = element_blank()) +
-    ggtitle("Performance metrics")
-  
-  PR_plot <- performance %>% 
-    ggplot(aes(x = Recall, y = Precision, colour = method_name, label = method)) +
-    geom_point() +
-    plot_settings +
-    ggrepel::geom_text_repel(max.overlaps = n_distinct(performance$method)) +
-    xlim(c(0,1)) + ylim(c(0,1)) + coord_equal() + 
-    ggtitle("Precision-Recall curve")
-  
-  out <- grid.arrange(
-    performance_plot,
-    PR_plot,
-    ncol = 2, nrow = 1,
-    top = textGrob(paste0("Method comparison (max score per CS)\n",
-                          plot_subtitle),
-                   gp = gpar(fontsize = 20)))
-  return(out)
-}
 
+# loop ====
 all_variants <- list() ; for(variants_name in 
     unique(read_tibble(paste0(base_dir, "/wrangle_package_data/traits/output/metadata.tsv"), header = T)$variants)
-    ){
+    ){ # for testing: # all_variants<-list();variants_name="BC_Michailidou2017_FM"
 
   cat(variants_name, "#############\n")
   out_dir <- paste0("output/", variants_name, "/")
     
   # get methods metadata ====
-  metadata <- read_tibble("output/metadata.tsv", header = T)
+  methods_metadata <- read_tibble("output/metadata.tsv", header = T)
   
   # get variants files ====
   variants_metadata <- paste0(base_dir, "/wrangle_package_data/traits/output/metadata.tsv") %>%
@@ -109,53 +43,104 @@ all_variants <- list() ; for(variants_name in
   known_genes <- read_tibble(variants_metadata$known_genes_file)$V1 %>%
     check_known_genes(variants_metadata$known_genes_file)
   variants <- import_BED(variants_metadata$variants_file, metadata_cols = c("variant", "cs"))
+  vxt_master <- get_vxt_master(variants, TSSs, variant_to_gene_max_distance)
+  
+  # get distance-based predictions ====
+  closest <- vxt_master %>%
+    group_by(cs, symbol) %>% 
+    filter(distance == min(distance)) %>%
+    transmute(
+      variant, cs, symbol,
+      score = case_when(distance == 0 ~ 1, TRUE ~ 1 / distance),
+      method = "closest")
   
   # get gene predictions ====
-  vxg <- read_tibble(
-    paste(out_dir, "predictions.tsv", sep = "/"),
-    header = T) 
+  annotations_in <- paste(out_dir, "predictions.tsv", sep = "/") %>%
+    read_tibble(header = T) %>%
+    bind_rows(closest) %>%
+    select(-variant)
+  annotations <- annotations_in %>%
+    right_join(
+      vxt_master %>%
+        distinct(cs, symbol) %>%
+        crossing(tibble(method = unique(annotations_in$method)))
+    ) %>%
+    mutate(score = replace_na(score, 0)) %>%
+    pivot_wider(id_cols = c(cs, symbol),
+                names_from = method,
+                values_from = score,
+                values_fn = max) 
   
-  # gene master list - protein coding genes annotated, nearby known genes annotated ====
-  cxg_universe <- variants %>%
-    get_vxt_master(TSSs, variant_to_gene_max_distance) %>%
-    mutate(known_gene = symbol %in% known_genes$symbol) %>%
-    # protein coding, only variants with kg within 2Mb
-    get_testable %>%
-    distinct(cs, symbol, known_gene) %>%
-    crossing(tibble(method = unique(vxg$method)))
+  # get performance ====
+  performance <- annotations %>%
+    get_PR(vxt_master, known_genes, pcENSGs, max_n_known_genes_per_CS) %>%
+    map(~ .x %>% mutate(method = prediction_method %>% gsub("\\_.*", "", .))) 
+  all_variants[[variants_name]] <- performance$summary
   
-  # cxg predictions in the cxg universe of the analysis
-  cxg_predictions <- vxg %>%
-    select(cs, symbol, score, method) %>%
-    group_by(cs, method) %>%
-    filter(score == max(score) & score > 0) %>%
-    mutate(max = T) %>%
-    # add all genes near CSs near known genes per method, GENCODE pc genes only
-    right_join(cxg_universe, by = c("cs", "symbol", "method")) %>%
-    mutate(across(where(is.logical), ~ replace_na(.x, FALSE)))
+  no_preds <- performance$summary %>% filter(Positive == 0)
+  if(nrow(no_preds) > 0){message(paste(no_preds$prediction_method,collapse=", "), "\n... has no positive predictions - check predictions file for errors!")}
+
+  # plot performance ====
+  colours_df <- methods_metadata %>% 
+    left_join(performance$summary %>% select(method, prediction_method)) %>%
+    filter(!is.na(prediction_method))
+  colours <- colours_df$colour
+  names(colours) <- colours_df$prediction_method
+  plot_settings <- list(
+    scale_colour_manual(values = colours, limits = force),
+    scale_fill_manual(values = colours, limits = force),
+    labs(x = paste0("recall (True = ", 
+                    unique(performance$summary$True), 
+                    ")")),
+    theme_classic(),
+    theme(
+      title = element_text(size = 15),
+      axis.title = element_text(size = 15),
+      axis.text = element_text(size = 15),
+      legend.position = "none"))
+  plot_subtitle <- paste0(
+    "Variants = ", variants_name,
+    "\nmax n known genes per CS = ", max_n_known_genes_per_CS, 
+    ", max distance = ", variant_to_gene_max_distance)
   
-  # performance ====
-  confusion_mat <- cxg_predictions %>%
-    group_by(method) %>%
-    get_group_conusion_mat
-  performance <- get_performance(confusion_mat, variants_name)
+  PR_plot <- performance %>%
+    map(~ filter(.x, prediction_type == "max")) %>% 
+    plot_PR(colour = prediction_method) +
+    ggrepel::geom_text_repel(data = . %>% 
+                               dplyr::filter(prediction_type == "max")) +
+    geom_line(data = performance$PR %>% filter(prediction_type == "score",
+                                               .threshold %ni% c(0, Inf, -Inf),
+                                               recall != 1,
+                                               precision != 0),
+              alpha = 0.3) +
+    plot_settings
+  performance_plot <- performance$summary %>%
+    mutate(fsc = F_score) %>%
+    pivot_longer(cols = c(F_score, Precision, Recall),
+                        names_to = "metric",
+                        values_to = "performance") %>%
+    ggplot(aes(x = reorder(prediction_method, fsc),
+                                 y = performance,
+                                 fill = prediction_method)) +
+    geom_col() +
+    facet_grid(~ metric,
+                        scales = "free", space = "free_y") +
+    coord_flip() +
+    theme(axis.title = element_blank()) +
+    plot_settings
   
-  all_variants[[variants_name]] <- confusion_mat
+  out_plot <- grid.arrange(
+    performance_plot,
+    PR_plot,
+    ncol = 2, nrow = 1,
+    top = textGrob(paste0("Method comparison (max score per CS)\n",
+                          plot_subtitle),
+                   gp = gpar(fontsize = 20)))
   
-  # plot_performance
-  performance_plot <- plot_performance(metadata, 
-                                       performance, 
-                                       variants_name, 
-                                       max_n_known_genes_per_CS,
-                                       variant_to_gene_max_distance)
-  
-  # write output
-  write_tibble(performance, 
-               paste0(out_dir, "performance.tsv"))
-  {
-    ggplot2::ggsave(paste0(out_dir, "performance.pdf"), performance_plot, width = 15)
-    ggplot2::ggsave(paste0(out_dir, "performance.png"), performance_plot, width = 15)
-  }
+  # write output ====
+  { write_tibble(performance$summary, paste0(out_dir, "performance.tsv"))
+    ggplot2::ggsave(paste0(out_dir, "performance.pdf"), out_plot, width = 15)
+    ggplot2::ggsave(paste0(out_dir, "performance.png"), out_plot, width = 15) }
   
 }
 
@@ -165,7 +150,7 @@ all_performance <- all_variants %>%
   group_by(method) %>%
   summarise(across(where(is.numeric), sum)) %>%
   get_performance(variants_name = "all_variants")
-all_performance_plot <- plot_performance(metadata, all_performance, variants_name = "all_variants",
+all_performance_plot <- plot_performance(methods_metadata, all_performance, variants_name = "all_variants",
                  max_n_known_genes_per_CS, variant_to_gene_max_distance)
 # write output
 write_tibble(all_performance, 
